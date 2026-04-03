a549660ad51a4e1feade31d24be3a6233bed0795
AndrewPitts1996 <apitts4030@sdsu.edu>
2026-04-03 13:39:22 -0700

    added removal of output file if it exists

--- run/run_parallel.py
+++ run/run_parallel.py
@@ -2,7 +2,10 @@ from multiprocessing import Pool
 import argparse
 import os
 
+import warnings
+
 from run.run_model import run_fitter
+from src.util.defaults import get_root_path
 
 
 def get_args():
@@ -18,5 +21,12 @@ if __name__ == "__main__":
     if n_realizations >= os.cpu_count():
         raise UserWarning(f"Number of realizations ({n_realizations}) is greater than the avaiable CPU cores.")
 
+    root = get_root_path()
+    datapath = os.path.join(root, 'output/data/data.h5')
+    if os.path.exists(datapath):
+        os.remove(datapath)
+
+    warnings.filterwarnings("error", message="function 'max' is not known to astropy's Quantity")
+    
     with Pool(processes=n_realizations) as pool:
         pool.map(run_fitter, range(n_realizations))
